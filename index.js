d6c1719f6da67b8e5ce6aad355a7550f5225c4a1
Thomas Sattlecker <thomas.sattlecker@gmail.com>
2017-01-26 13:43:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const { send, json } = require('micro');
+const botkit = require('botkit');
+const SlackBot = require('slackbots');
+const createMessage = require('./messages');
+require('now-logs')('github-slack-bot-secret-1');
+
+let whitelist = [];
+if (!process.env.slacktoken) {
+  console.log('Error: Specify token in environment');
+  throw new Error('Error: Specify token in environment');
+}
+if (!process.env.nowtoken) {
+  console.log('Error: Specify token in environment');
+  throw new Error('Error: Specify token in environment');
+}
+if (!process.env.whitelist) {
+  console.log('Error: whitelist users');
+  throw new Error('Error: whitelist users');
+} else {
+  whitelist = process.env.whitelist.split(';').map(pair => {
+    let splitPair = pair.split(':');
+    return { key: splitPair[0], value: splitPair[1] };
+  });
+  whitelist = whitelist.reduce((prev, curr) => {
+    prev[curr.key] = curr.value;
+    return prev;
+  }, {});
+}
+
+const controller = botkit.slackbot({
+  debug: false,
+  scopes: [ 'bot', 'users:read' ]
+});
+
+const apiBot = new SlackBot({ token: process.env.slacktoken, name: 'gitbot' });
+
+apiBot.getUsers().then(users => {
+  let slackNames = Object.keys(whitelist).map(key => {
+    return whitelist[key];
+  });
+  this.users = users.members.filter(user => {
+    return slackNames.indexOf(user.name) > -1;
+  });
+});
+
+apiBot.on('error', function(data) {
+  console.log(data);
+});
+
+const bot = controller
+  .spawn({ token: process.env.slacktoken, scopes: [ 'bot', 'users:read' ] })
+  .startRTM();
+
+module.exports = async (req, res) => {
+  const body = await json(req);
+  let slackSenderId = whitelist[body.sender.login];
+  this.users
+    .map(user => {
+      return user;
+    })
+    .filter(user => user.name !== slackSenderId)
+    .map(users => {
+      return users;
+    })
+    .forEach(user => {
+      bot.startPrivateConversation({ user: user.id }, (err, convo) => {
+        convo.say(createMessage(body, req.headers['x-github-event']));
+      });
+    });
+  send(res, 200, body.action || 'something happened but i dont know what');
+};
---
+++ messages.js
@@ -0,0 +1,130 @@
+module.exports = function createMessage(body, event) {
+  switch (event) {
+    case 'commit_comment':
+      return createdComment(body);
+    case 'pull_request':
+      switch (body.action) {
+        case 'opened':
+        case 'edited':
+        case 'closed':
+        case 'reopened':
+          return createdPullrequest(body);
+        default:
+          break;
+      }
+    case 'pull_request_review':
+      return createdPullrequestReview(body);
+    case 'pull_request_review_comment':
+      return createdPullrequestReviewComment(body);
+    case 'issues':
+      switch (body.action) {
+        case 'opened':
+        case 'edited':
+        case 'closed':
+        case 'reopened':
+          return issue(body);
+        default:
+          break;
+      }
+    case 'issue_comment':
+      return issueComment(body);
+    default:
+      break;
+  }
+};
+
+function createdComment(body) {
+  return {
+    attachments: [
+      {
+        fields: [ { value: body.comment.body.substring(0, 200) + '...' } ],
+        title: body.repository.name,
+        title_link: body.comment.html_url,
+        fallback: `New comment in ${body.repository.name} by ${body.comment.user.login}`,
+        mrkdwn_in: [ 'text', 'pretext' ],
+        color: 'warning',
+        text: `Comment *${body.action}* in <${body.repository.html_url}|${body.repository.full_name}> by <${body.comment.user.html_url}|${body.comment.user.login}>`
+      }
+    ]
+  };
+}
+
+function createdPullrequest(body) {
+  return {
+    attachments: [
+      {
+        fields: [ { value: body.pull_request.body.substring(0, 200) + '...' } ],
+        title: `${body.pull_request.number}: ${body.pull_request.title}`,
+        title_link: body.pull_request.html_url,
+        fallback: `New Pullrequest in ${body.repository.name} by ${body.pull_request.user.login}`,
+        mrkdwn_in: [ 'text', 'pretext' ],
+        color: '#0A4CA1',
+        text: `Pullrequest *${body.action}* in <${body.repository.html_url}|${body.repository.full_name}> by <${body.pull_request.user.html_url}|${body.pull_request.user.login}>`
+      }
+    ]
+  };
+}
+
+function createdPullrequestReview(body) {
+  return {
+    attachments: [
+      {
+        fields: [ { value: body.review.body.substring(0, 200) + '...' } ],
+        title: `${body.pull_request.number}: ${body.pull_request.title}`,
+        title_link: body.review.html_url,
+        fallback: `New Review in ${body.repository.name} by ${body.review.user.login}`,
+        mrkdwn_in: [ 'text', 'pretext' ],
+        color: '#0A4CA1',
+        text: `Pullrequest *${body.review.state}* in <${body.repository.html_url}|${body.repository.title}> by <${body.review.user.html_url}|${body.review.user.login}>`
+      }
+    ]
+  };
+}
+
+function createdPullrequestReviewComment(body) {
+  return {
+    attachments: [
+      {
+        fields: [ { value: body.comment.body.substring(0, 200) + '...' } ],
+        title: `${body.pull_request.number}: ${body.pull_request.title}`,
+        title_link: body.comment.html_url,
+        fallback: `New comment in ${body.pull_request.title} by ${body.comment.user.login}`,
+        mrkdwn_in: [ 'text', 'pretext' ],
+        color: '#0A4CA1',
+        text: `Pullrequest *<${body.comment.html_url}|commented>* in <${body.repository.html_url}|${body.repository.full_name}> by <${body.comment.user.html_url}|${body.comment.user.login}>`
+      }
+    ]
+  };
+}
+
+function issue(body) {
+  return {
+    attachments: [
+      {
+        fields: [ { value: body.issue.body.substring(0, 200) + '...' } ],
+        title: `${body.issue.number}: ${body.issue.title}`,
+        title_link: body.issue.html_url,
+        fallback: `${body.issue.user.login} ${body.action} issue ${body.issue.title}`,
+        mrkdwn_in: [ 'text', 'pretext' ],
+        color: '#0AA10C',
+        text: `Issue *${body.action}* in <${body.repository.html_url}|${body.repository.title}> by <${body.issue.user.html_url}|${body.issue.user.login}>`
+      }
+    ]
+  };
+}
+
+function issueComment(body) {
+  return {
+    attachments: [
+      {
+        fields: [ { value: body.issue.body.substring(0, 200) + '...' } ],
+        title: `${body.issue.number}: ${body.issue.title}`,
+        title_link: body.comment.html_url,
+        fallback: `New comment in ${body.issue.title} by ${body.comment.user.login}`,
+        mrkdwn_in: [ 'text', 'pretext' ],
+        color: '#0AA10C',
+        text: `Issue Comment *${body.action}* in <${body.repository.html_url}|${body.repository.title}> by <${body.issue.user.html_url}|${body.issue.user.login}>`
+      }
+    ]
+  };
+}
