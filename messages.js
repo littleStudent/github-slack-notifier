c801c38452254c75cad0150ac94a52de29ad8072
Thomas Sattlecker <thomas.sattlecker@gmail.com>
2017-01-27 10:49:22 +0100

    improved fallback notification text

--- messages.js
+++ messages.js
@@ -40,7 +40,7 @@ function createdComment(body) {
         fields: [ { value: body.comment.body.substring(0, 100) + '...' } ],
         title: body.repository.name,
         title_link: body.comment.html_url,
-        fallback: `New comment in ${body.repository.name} by ${body.comment.user.login}`,
+        fallback: `Comment ${body.action} in ${body.repository.full_name} by ${body.comment.user.login}`,
         mrkdwn_in: [ 'text', 'pretext' ],
         color: 'warning',
         text: `Comment *${body.action}* in <${body.repository.html_url}|${body.repository.full_name}> by <${body.comment.user.html_url}|${body.comment.user.login}>`
@@ -56,7 +56,7 @@ function createdPullrequest(body) {
         fields: [ { value: body.pull_request.body.substring(0, 100) + '...' } ],
         title: `${body.pull_request.number}: ${body.pull_request.title}`,
         title_link: body.pull_request.html_url,
-        fallback: `New Pullrequest in ${body.repository.name} by ${body.pull_request.user.login}`,
+        fallback: `Pullrequest ${body.action} in ${body.repository.full_name} by ${body.pull_request.user.login}`,
         mrkdwn_in: [ 'text', 'pretext' ],
         color: '#0A4CA1',
         text: `Pullrequest *${body.action}* in <${body.repository.html_url}|${body.repository.full_name}> by <${body.pull_request.user.html_url}|${body.pull_request.user.login}>`
@@ -72,7 +72,7 @@ function createdPullrequestReview(body) {
         fields: [ { value: body.review.body.substring(0, 100) + '...' } ],
         title: `${body.pull_request.number}: ${body.pull_request.title}`,
         title_link: body.review.html_url,
-        fallback: `New Review in ${body.repository.name} by ${body.review.user.login}`,
+        fallback: `Pullrequest ${body.review.state} in ${body.repository.name} by ${body.review.user.login}`,
         mrkdwn_in: [ 'text', 'pretext' ],
         color: '#0A4CA1',
         text: `Pullrequest *${body.review.state}* in <${body.repository.html_url}|${body.repository.name}> by <${body.review.user.html_url}|${body.review.user.login}>`
@@ -88,7 +88,7 @@ function createdPullrequestReviewComment(body) {
         fields: [ { value: body.comment.body.substring(0, 100) + '...' } ],
         title: `${body.pull_request.number}: ${body.pull_request.title}`,
         title_link: body.comment.html_url,
-        fallback: `New comment in ${body.pull_request.title} by ${body.comment.user.login}`,
+        fallback: `Pullrequest commented in ${body.repository.full_name} by ${body.comment.user.login}`,
         mrkdwn_in: [ 'text', 'pretext' ],
         color: '#0A4CA1',
         text: `Pullrequest *<${body.comment.html_url}|commented>* in <${body.repository.html_url}|${body.repository.full_name}> by <${body.comment.user.html_url}|${body.comment.user.login}>`
@@ -104,7 +104,7 @@ function issue(body) {
         fields: [ { value: body.issue.body.substring(0, 100) + '...' } ],
         title: `${body.issue.number}: ${body.issue.title}`,
         title_link: body.issue.html_url,
-        fallback: `${body.issue.user.login} ${body.action} issue ${body.issue.title}`,
+        fallback: `Issue ${body.action} in ${body.repository.name} by ${body.issue.user.login}`,
         mrkdwn_in: [ 'text', 'pretext' ],
         color: '#0AA10C',
         text: `Issue *${body.action}* in <${body.repository.html_url}|${body.repository.name}> by <${body.issue.user.html_url}|${body.issue.user.login}>`
@@ -120,7 +120,7 @@ function issueComment(body) {
         fields: [ { value: body.issue.body.substring(0, 100) + '...' } ],
         title: `${body.issue.number}: ${body.issue.title}`,
         title_link: body.comment.html_url,
-        fallback: `New comment in ${body.issue.title} by ${body.comment.user.login}`,
+        fallback: `Issue Comment ${body.action} in ${body.repository.name} by ${body.issue.user.login}`,
         mrkdwn_in: [ 'text', 'pretext' ],
         color: '#0AA10C',
         text: `Issue Comment *${body.action}* in <${body.repository.html_url}|${body.repository.name}> by <${body.issue.user.html_url}|${body.issue.user.login}>`
